0a6cf707c90ca246d1338c0081b1ca59698e36fe
Markus Keil <markus.keil@slock.it>
2019-02-21 15:58:38 +0100

    migrate doc

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # system-design-spec-live
 System requirements specification document
+
+
+## Build / Render PDF
+
+you need:
+* NodeJS/npm
+* graphviz (dot)
+- docker
+
+run `sh build.sh` to render the digarmas and the LaTeX.
---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "installing stuff"
+npm install
+echo "Building diagrams..."
+node_modules/.bin/diagrams sequence diagrams/mgnt-gui-boot.sequence build/mgnt-gui-boot.png
+dot -Tpng -obuild/flow-onboard-validator.png diagrams/flow-onboard-validator.dot
+dot -Tpng -obuild/flow-update-validator.png diagrams/flow-update-validator.dot
+dot -Tpng -obuild/sys-diagram.png diagrams/sys-diagram.dot
+dot -Tpng -obuild/genesis-network.png diagrams/genesis-network.dot
+dot -Tpng -obuild/nodecontrol.png diagrams/nodecontrol.dot
+
+cp build/*.png src/images
+
+CURDIR=`pwd`
+shopt -s extglob
+docker run -it --rm --mount src=$CURDIR/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /usr/bin/tectronic -p main.tex
+
+echo "doc in src/main.pdf"
+
+#xdg-open src/main.pdf
